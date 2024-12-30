857dd5fdcd0b0b626bea039a2416fa28355351f4
Harshit Sharma <123459218+HsAhRaSrHmIaT@users.noreply.github.com>
2024-12-30 16:26:16 +0530

    Database changed to mysql

--- Back/todo/settings.py
+++ Back/todo/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "todo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "todo_django_db",
+        "USER": "harshit",
+        "PASSWORD": "cca6140955ee",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
